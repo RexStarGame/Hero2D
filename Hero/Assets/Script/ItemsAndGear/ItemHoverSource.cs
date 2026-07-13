10284ee4a8760033905906ac567585ecea103c0c
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-07-13 21:43:55 +0200

    Generalize equipment and inventory systems

--- Hero/Assets/Script/ItemsAndGear/ItemHoverSource.cs
+++ Hero/Assets/Script/ItemsAndGear/ItemHoverSource.cs
@@ -6,7 +6,7 @@ public class ItemHoverSource : MonoBehaviour, IPointerEnterHandler, IPointerExit
     [SerializeField] private ItemDefinition item;
     [SerializeField] private bool equipped;
     public void SetItem(ItemDefinition value, bool isEquipped) { item = value; equipped = isEquipped; }
-    public void OnPointerEnter(PointerEventData e) { if (RingSlotUI.DraggedSlot == null) ItemTooltipUI.Instance?.Show(item, e.position, equipped); }
+    public void OnPointerEnter(PointerEventData e) { if (!ItemDragContext.IsDragging && RingSlotUI.DraggedSlot == null) ItemTooltipUI.Instance?.Show(item, e.position, equipped); }
     public void OnPointerMove(PointerEventData e) { ItemTooltipUI.Instance?.Move(e.position); }
     public void OnPointerExit(PointerEventData e) { ItemTooltipUI.Instance?.Hide(); }
 }
