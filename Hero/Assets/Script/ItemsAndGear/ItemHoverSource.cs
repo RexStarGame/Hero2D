fdf05fa407a27be0f2224317f46282c0b7225ce1
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-07-14 20:55:33 +0200

    Clear item tooltip when hover source closes

--- Hero/Assets/Script/ItemsAndGear/ItemHoverSource.cs
+++ Hero/Assets/Script/ItemsAndGear/ItemHoverSource.cs
@@ -5,8 +5,36 @@ public class ItemHoverSource : MonoBehaviour, IPointerEnterHandler, IPointerExit
 {
     [SerializeField] private ItemDefinition item;
     [SerializeField] private bool equipped;
-    public void SetItem(ItemDefinition value, bool isEquipped) { item = value; equipped = isEquipped; }
-    public void OnPointerEnter(PointerEventData e) { if (!ItemDragContext.IsDragging && RingSlotUI.DraggedSlot == null) ItemTooltipUI.Instance?.Show(item, e.position, equipped); }
-    public void OnPointerMove(PointerEventData e) { ItemTooltipUI.Instance?.Move(e.position); }
-    public void OnPointerExit(PointerEventData e) { ItemTooltipUI.Instance?.Hide(); }
+
+    public void SetItem(ItemDefinition value, bool isEquipped)
+    {
+        item = value;
+        equipped = isEquipped;
+
+        if (item == null)
+            ItemTooltipUI.Instance?.Hide();
+    }
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        if (item != null && !ItemDragContext.IsDragging && RingSlotUI.DraggedSlot == null)
+            ItemTooltipUI.Instance?.Show(item, e.position, equipped);
+    }
+
+    public void OnPointerMove(PointerEventData e)
+    {
+        if (item != null && !ItemDragContext.IsDragging)
+            ItemTooltipUI.Instance?.Move(e.position);
+    }
+
+    public void OnPointerExit(PointerEventData e)
+    {
+        ItemTooltipUI.Instance?.Hide();
+    }
+
+    private void OnDisable()
+    {
+        // Unity may disable the inventory before it sends OnPointerExit.
+        ItemTooltipUI.Instance?.Hide();
+    }
 }
