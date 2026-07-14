df001a8ef72819e103ca60430af074e5da08dfa2
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-07-14 20:07:59 +0200

    Complete equipment slot drag and drop feedback

--- Hero/Assets/Script/ItemsAndGear/EquipmentSlotUI.cs
+++ Hero/Assets/Script/ItemsAndGear/EquipmentSlotUI.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class EquipmentSlotUI : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDropHandler, IPointerClickHandler
+public class EquipmentSlotUI : MonoBehaviour,
+    IBeginDragHandler, IDragHandler, IEndDragHandler,
+    IDropHandler, IPointerClickHandler
 {
     [SerializeField] private EquipmentSlotType slotType;
     [SerializeField] private int slotNumber;
@@ -14,34 +16,139 @@ public class EquipmentSlotUI : MonoBehaviour, IBeginDragHandler, IEndDragHandler
     [SerializeField] private TMP_Text emptyLabel;
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private ItemHoverSource hover;
+
     public EquipmentSlotType SlotType => slotType;
     public int SlotNumber => slotNumber;
     public EquippableItemDefinition Item => equipment?.GetItem(slotType, slotNumber);
 
-    private void Awake() { if (canvasGroup == null) canvasGroup = GetComponent<CanvasGroup>(); }
-    private void OnEnable() { if (equipment != null) equipment.EquipmentChanged += Refresh; Refresh(); }
-    private void OnDisable() { if (equipment != null) equipment.EquipmentChanged -= Refresh; }
+    private void Awake()
+    {
+        if (canvasGroup == null)
+            canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void OnEnable()
+    {
+        if (equipment != null)
+            equipment.EquipmentChanged += Refresh;
+
+        Refresh();
+    }
+
+    private void OnDisable()
+    {
+        if (equipment != null)
+            equipment.EquipmentChanged -= Refresh;
+    }
+
     public void Refresh()
     {
         EquippableItemDefinition item = Item;
-        if (icon != null) { icon.enabled = item != null; icon.sprite = item == null ? null : item.Icon; }
-        if (emptyLabel != null) emptyLabel.gameObject.SetActive(item == null);
-        if (validHighlight != null) validHighlight.enabled = false;
-        if (hover != null) hover.SetItem(item, true);
+
+        if (icon != null)
+        {
+            icon.enabled = item != null;
+            icon.sprite = item == null ? null : item.Icon;
+        }
+
+        if (emptyLabel != null)
+            emptyLabel.gameObject.SetActive(item == null);
+
+        if (validHighlight != null)
+            validHighlight.enabled = false;
+
+        if (hover != null)
+            hover.SetItem(item, true);
     }
-    public void OnBeginDrag(PointerEventData e) { if (Item == null) return; ItemDragContext.Begin(this); if (canvasGroup != null) canvasGroup.blocksRaycasts = false; SetHighlights(true, Item); }
-    public void OnEndDrag(PointerEventData e) { if (canvasGroup != null) canvasGroup.blocksRaycasts = true; SetHighlights(false, null); ItemDragContext.Clear(); }
-    public void OnDrop(PointerEventData e)
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (Item == null) return;
+
+        ItemDragContext.Begin(this);
+        ItemDragVisualUI.Instance?.Show(Item, eventData.position);
+
+        if (canvasGroup != null)
+            canvasGroup.blocksRaycasts = false;
+
+        SetHighlights(true, Item);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ItemDragVisualUI.Instance?.Move(eventData.position);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        bool droppedOutside = !ItemDragContext.DropHandled &&
+            InventoryPanelUI.Instance != null &&
+            !InventoryPanelUI.Instance.IsScreenPointInside(
+                eventData.position,
+                eventData.pressEventCamera);
+
+        if (droppedOutside)
+            inventory?.DropEquippedItemToWorld(slotType, slotNumber);
+
+        if (canvasGroup != null)
+            canvasGroup.blocksRaycasts = true;
+
+        SetHighlights(false, null);
+        ItemDragVisualUI.Instance?.Hide();
+        ItemDragContext.Clear();
+    }
+
+    public void OnDrop(PointerEventData eventData)
     {
         if (ItemDragContext.InventorySource != null)
-            inventory.EquipFromInventory(ItemDragContext.InventorySource.InventoryIndex, slotType, slotNumber);
-        else if (ItemDragContext.EquipmentSource != null && slotType == EquipmentSlotType.Ring && ItemDragContext.EquipmentSource.slotType == EquipmentSlotType.Ring)
+        {
+            bool equippedItem = inventory != null &&
+                inventory.EquipFromInventory(
+                    ItemDragContext.InventorySource.InventoryIndex,
+                    slotType,
+                    slotNumber);
+
+            if (equippedItem)
+                ItemDragContext.MarkDropHandled();
+
+            return;
+        }
+
+        EquipmentSlotUI source = ItemDragContext.EquipmentSource;
+        if (source == null) return;
+
+        if (source == this)
+        {
+            ItemDragContext.MarkDropHandled();
+            return;
+        }
+
+        if (slotType == EquipmentSlotType.Ring &&
+            source.slotType == EquipmentSlotType.Ring &&
+            equipment != null)
+        {
             equipment.SwapRings();
+            ItemDragContext.MarkDropHandled();
+        }
     }
-    public void OnPointerClick(PointerEventData e) { if (e.button == PointerEventData.InputButton.Right) inventory.UnequipToInventory(slotType, slotNumber); }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+            inventory?.UnequipToInventory(slotType, slotNumber);
+    }
+
     public static void SetHighlights(bool visible, EquippableItemDefinition dragged)
     {
         foreach (EquipmentSlotUI slot in FindObjectsByType<EquipmentSlotUI>(FindObjectsSortMode.None))
-            if (slot.validHighlight != null) slot.validHighlight.enabled = visible && dragged != null && dragged.EquipmentSlot == slot.slotType;
+        {
+            if (slot.validHighlight != null)
+            {
+                slot.validHighlight.enabled =
+                    visible &&
+                    dragged != null &&
+                    dragged.EquipmentSlot == slot.slotType;
+            }
+        }
     }
 }
