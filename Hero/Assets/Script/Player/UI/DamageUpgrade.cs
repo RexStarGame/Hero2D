aa6654052ccf7ca412d6187e1f3ebb9cfbca5437
Patrick <63421519+RexStarGame@users.noreply.github.com>
2026-08-09 19:42:46 +0200

    Remove unused respec helper property

--- Hero/Assets/Script/Player/UI/DamageUpgrade.cs
+++ Hero/Assets/Script/Player/UI/DamageUpgrade.cs
@@ -55,7 +55,6 @@ public class DamageUpgrade : MonoBehaviour, ISerializationCallbackReceiver
     public int Damage => MaximumDamage;
 
     public int DamageLevel => damageLevel;
-    public int UpgradeCost => Mathf.Max(0, cost);
 
     public int RollDamage()
     {
